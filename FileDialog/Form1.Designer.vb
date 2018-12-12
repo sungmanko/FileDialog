ca8a1556c43963e95a48d10e6b874175b0f81c3e
sungman ko <33561581+sungmanko@users.noreply.github.com>
2018-12-12 13:13:47 +0900

    FileDialog
    FileDialog

---
+++ FileDialog/Form1.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnFileSelect = New System.Windows.Forms.Button()
+        Me.btnFileCopy = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'btnFileSelect
+        '
+        Me.btnFileSelect.Location = New System.Drawing.Point(252, 22)
+        Me.btnFileSelect.Name = "btnFileSelect"
+        Me.btnFileSelect.Size = New System.Drawing.Size(75, 23)
+        Me.btnFileSelect.TabIndex = 0
+        Me.btnFileSelect.Text = "파일선택"
+        Me.btnFileSelect.UseVisualStyleBackColor = True
+        '
+        'btnFileCopy
+        '
+        Me.btnFileCopy.Location = New System.Drawing.Point(252, 67)
+        Me.btnFileCopy.Name = "btnFileCopy"
+        Me.btnFileCopy.Size = New System.Drawing.Size(75, 23)
+        Me.btnFileCopy.TabIndex = 1
+        Me.btnFileCopy.Text = "파일복사"
+        Me.btnFileCopy.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 24)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(234, 19)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 71)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(234, 19)
+        Me.TextBox2.TabIndex = 3
+        Me.TextBox2.Text = "C:\test\"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(340, 114)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.btnFileCopy)
+        Me.Controls.Add(Me.btnFileSelect)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btnFileSelect As Button
+    Friend WithEvents btnFileCopy As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+End Class
---
+++ FileDialog/Form1.vb
@@ -0,0 +1,83 @@
+﻿Public Class Form1
+
+    ''' <summary>
+    ''' 파일을 선택합니다.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnFileSelect_Click(sender As Object, e As EventArgs) Handles btnFileSelect.Click
+
+        Dim OpenFileDialog1 As New OpenFileDialog()
+        OpenFileDialog1.Title = "파일선택 타이틀입니다."
+
+        ' 초기 디렉토리를 설정합니다.
+        OpenFileDialog1.InitialDirectory = "C:\"
+
+        ' 초기 선택파일명을 선택합니다.
+        OpenFileDialog1.FileName = "초기파일명"
+
+        ' 파일의 필터를 설정합니다.
+        OpenFileDialog1.Filter = "텍스트파일|*.txt;*.log|전체파일|*.*"
+
+        ' 필터의 초기설정입니다. 
+        OpenFileDialog1.FilterIndex = 1
+
+        ' 다이얼로그를 닫을때 마지막 설정을 유효로 하겠다는 설정입니다.
+        OpenFileDialog1.RestoreDirectory = True
+
+        ' 복수파일을 선택가능하게 합니다.
+        OpenFileDialog1.Multiselect = False
+
+        ' HELP 버튼을 표시합니다.
+        OpenFileDialog1.ShowHelp = True
+
+        ' 읽기전용으로 표시합니다.
+        OpenFileDialog1.ShowReadOnly = True
+
+        ' 체크박스를 표시합니다.
+        OpenFileDialog1.ReadOnlyChecked = True
+
+        ' 존재하지않는 파일을 선택했을때 경고를 표시합니다.
+        'OpenFileDialog1.CheckFileExists = True
+
+        ' 존재하지않는 경로를 지정했을때 경고를 표시합니다.
+        'OpenFileDialog1.CheckPathExists = True
+
+        ' 확장자가 존재하지 않는경우에는 확장자를 자동으로 붙여줍니다.
+        'OpenFileDialog1.AddExtension = True
+
+        ' 유효한 Win32 파일명만을 입력받게합니다.
+        'OpenFileDialog1.ValidateNames = True
+
+        ' 다이얼로그를 표시하고 OK 선택시에만 선택한 파일을 표시합니다.
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            TextBox1.Text = OpenFileDialog1.FileName
+
+            ' 파일이 복수선택 되었을때는 아래의 로직이 돌아가며 뿌려줍니다.
+            'For Each nFileName As String In OpenFileDialog1.FileNames
+            '    MessageBox.Show(nFileName)
+            'Next nFileName
+        End If
+
+        ' 처리가 끝났으므로 파기합니다.
+        OpenFileDialog1.Dispose()
+    End Sub
+
+    ''' <summary>
+    ''' 파일을 지정된곳으로 복사합니다.
+    ''' </summary>
+    ''' <param name="sender"></param>
+    ''' <param name="e"></param>
+    Private Sub btnFileCopy_Click(sender As Object, e As EventArgs) Handles btnFileCopy.Click
+
+        ' 파일명을 취득합니다.
+        Dim fileName As String = System.IO.Path.GetFileName(TextBox1.Text)
+
+        ' 파일을 복사합니다.
+        System.IO.File.Copy(TextBox1.Text, TextBox2.Text & fileName, True)
+
+        ' 파일을 이동합니다.
+        'System.IO.File.Move("C:\test\1.txt", "C:\test\3.txt")
+    End Sub
+
+End Class
